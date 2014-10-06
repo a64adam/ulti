56fbb763f8cd5e31d628cd0f5f248ee459c72623
Adam Alyyan <a64adam@gmail.com>
2014-10-05 23:26:42 -0700

    Removed personal api key from public branch

--- src/test/java/api/UltiTest.java
+++ src/test/java/api/UltiTest.java
@@ -40,7 +40,7 @@ public class UltiTest {
 
     @BeforeClass
     public static void setUp() throws Exception {
-        ulti = new Ulti("db012a8a-e9c0-413d-9c97-9dffe07a91c5");
+        ulti = new Ulti("YOUR_API_KEY");
         summonerId = 23287684l;
         teamId = "TEAM-fe3c3290-28e4-11e4-91d2-782bcb4d0bb2";
     }
